554e990bb07b884e703945c6226698f9474d0f1b
anajulianp <anajuliaestudantil@gmail.com>
2026-03-11 17:31:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 tasks = []
 
 while True:
-  print("\nGerenciador de tarefas:")
-  print("1- Ver tarefas")
-  print("2- Adicionar tarefas")
-  print("3- Sair")
+    print("\nGerenciador de tarefas:")
+    print("1- Ver tarefas")
+    print("2- Adicionar tarefas")
+    print("3- Sair")
+    
+    choice = input("Escolha uma opção: ")
 
-choice = input("Escolha uma opção: ")
+    if choice == "1":
+        print(tasks)
 
-if choise == "1":
-  print(tasks)
+    elif choice == "2":
+        task = input("Digite a tarefa: ")
+        tasks.append(task)
 
-elif choise == "2":
-  task = input("Digite a tarefa: ")
-  tasks.append(task)
+    elif choice == "3":
+        print("Saindo do programa...")
+        break
 
-elif choice == "3":
-  break
+    else:
+        print("Opção inválida, tente novamente.")
   
