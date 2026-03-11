0d35e4c75de02698cdd8f808b0b87e2c3ba1f21b
anajulianp <anajuliaestudantil@gmail.com>
2026-03-11 16:58:50 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+tasks []
+
+while True:
+  print ("\nGerenciador de tarefas:")
+  print("1- Ver tarefas")
+  print("2- Adicionar tarefas")
+  print("3- Sair")
+
+choice = input("Escolha uma opção: ")
+
+if choise == "1":
+  print(tasks)
+
+elif choise == "2":
+  task = input("Digite a tarefa: ")
+  task.append(task)
+
+elif choise == "3":
+  break
+  
